a3cd1475794d181822e192b1237d2273edd31a33
luhaiming <luhaiming@zillionfortune.com>
2018-03-02 17:11:24 +0800

    add

--- app/src/main/java/com/sh/jackie/smartstock/MainActivity.java
+++ app/src/main/java/com/sh/jackie/smartstock/MainActivity.java
@@ -1,13 +1,34 @@
 package com.sh.jackie.smartstock;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Date;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn  = findViewById(R.id.btn1);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                final TextView show  = findViewById(R.id.show);
+                show.setText("btn clicked "+new Date());
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 }
